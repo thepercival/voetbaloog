d1b2b162a4a731a79bbe28a75286de99b64e807b
thepercival <coendunnink@gmail.com>
2024-06-26 15:26:56 +0200

    make final changes

--- voetbaloog-old/post_svn_update.sh
+++ voetbaloog-old/post_svn_update.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 CURRENTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PRIVATEHTML=/var/www/voetbaloog
+#PRIVATEHTML=$CURRENTPATH
+#PRIVATEHTML=/var/www/voetbaloog
 
+echo $CURRENTPATH
 # do dbupdates and autorizationupdates
-php $PRIVATEHTML/app_voetbaloog/model/installer/update.php
+XDEBUG_MODE=off php $CURRENTPATH/app_voetbaloog/model/installer/update.php
 
 # shrink jslibs jslibrary and jslibraryvo
-PUBLICHTML=/var/www/voetbaloog/pub_voetbaloog
-YUIJAR=$PRIVATEHTML/ThirdParties/yuicompressor-2.4.8.jar
+PUBLICHTML=$CURRENTPATH/pub_voetbaloog
+YUIJAR=$CURRENTPATH/ThirdParties/yuicompressor-2.4.8.jar
 MAPDIR=$PUBLICHTML/public/scripts
 JSTHPDIR=$PUBLICHTML/jsthirdparties/jsvoetbal/jquery
 MINIFIEDJS=$MAPDIR/custom.min.js
 COMBINEDJS=$MAPDIR/combined.js
 
-# scripts
+echo "javascript combineren"
 if [ -e $MINIFIEDJS ]; then rm $MINIFIEDJS; fi
 find $MAPDIR -type f \( -name "custom.js" \) -exec cat {} \; >> $COMBINEDJS.tmp
 find $JSTHPDIR -type f \( -name "voetbal.js" \) -exec cat {} \; >> $COMBINEDJS.tmp
@@ -25,6 +27,10 @@ rm $COMBINEDJS
 java -jar $YUIJAR --type js $MAPDIR/lazyload.js -o $MAPDIR/lazyload.js.tmp
 rm $MAPDIR/lazyload.js
 mv $MAPDIR/lazyload.js.tmp $MAPDIR/lazyload.js
+echo "javascript neergezet op $MAPDIR/lazyload.js"
+
+exit 
+return
 
 # jslibrary
 LIBDIR=$PUBLICHTML/jslibrary
@@ -61,7 +67,7 @@ mv $COMBINEDCSS.tmp $COMBINEDCSS
 java -jar $YUIJAR --type css $LIBDIR/$COMBINEDCSS -o $MINIFIEDCSS
 rm $COMBINEDCSS
 
-rm $PRIVATEHTML/app_voetbaloog/cache/*
+rm $CURRENTPATH/app_voetbaloog/cache/*
 
 cd $CURRENTPATH
 exit
